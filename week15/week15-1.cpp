2ac7eb8d8f71c272efc658b7442f4b191011f5ae
ning092 <11160073@mail.mcu.edu.tw>
2022-12-16 11:59:51 +0800

    week15

---
+++ week15/week15-1.cpp
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+
+    printf("Hello World\n");
+    char line[] = "Hello World";
+    printf("整數 %\n",100);
+    printf("浮點數%f\n", 3.141592653589793238462643383279);
+    printf("%s字串\n", line);
+
+}
